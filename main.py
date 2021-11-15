762a85bcbb13458f705b355ec167951ed57a71d2
Avinash <vintas@users.noreply.github.com>
2021-11-15 18:42:51 +0530

    stick to an input box till the next keyword

--- main.py
+++ main.py
@@ -47,13 +47,14 @@ def fill_input_box(form_input_text_name, form_input_text_speech_transcribed):
 def submit_visit_slip():
     submit_button_element = driver.find_element(by='id', value='submit')
     submit_button_element.click()
-    time.sleep(5)  # Let the user actually see something!
+    time.sleep(10)  # Let the user actually see something!
     driver.quit()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     open_patient_visit_slip()
+    form_input_text_name = ''
     while (1):
         # Exception handling to handle
         # exceptions at the runtime
@@ -64,7 +65,7 @@ if __name__ == '__main__':
                 # adjust the energy threshold based on
                 # the surrounding noise level
                 r.adjust_for_ambient_noise(source2, duration=0.2)
-                print('here')
+                # print('here')
                 # listens for the user's input
                 audio2 = r.listen(source2)
 
@@ -77,21 +78,25 @@ if __name__ == '__main__':
                     form_input_text_name = 'history'
                     form_input_text_speech_transcribed = MyText
                     print("his : " + MyText)  # call function to move to history
-                    fill_input_box(form_input_text_name, form_input_text_speech_transcribed)
-                if 'diagnosis' in MyText.split(' '):
+                    # fill_input_box(form_input_text_name, form_input_text_speech_transcribed)
+                if 'diagnosis' in MyText.split(' ') or 'diagnose'in MyText.split(' '):
                     form_input_text_name = 'diagnosis'
                     form_input_text_speech_transcribed = MyText
                     print("diag : " + MyText)  # call function to move to diag
-                    fill_input_box(form_input_text_name, form_input_text_speech_transcribed)
-                if 'prescription' in MyText.split(' '):
+                    # fill_input_box(form_input_text_name, form_input_text_speech_transcribed)
+                if 'prescription' in MyText.split(' ') or 'prescribe' in MyText.split(' '):
                     form_input_text_name = 'prescription'
                     form_input_text_speech_transcribed = MyText
                     print("pres : " + MyText)  # call function to move to pres
-                    fill_input_box(form_input_text_name, form_input_text_speech_transcribed)
-                if 'submit' in MyText.split(' '):
+                    # fill_input_box(form_input_text_name, form_input_text_speech_transcribed)
+                if 'submit' in MyText.split(' ') or 'save' in MyText.split(' '):
                     print("submit")
                     submit_visit_slip()
                     break
+                print(form_input_text_name + " : " + MyText)
+                if form_input_text_name:
+                    fill_input_box(form_input_text_name, MyText)
+
 
         except sr.RequestError as e:
             print("Could not request results; {0}".format(e))
